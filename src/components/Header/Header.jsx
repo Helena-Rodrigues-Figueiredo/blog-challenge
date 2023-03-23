5f4d36334cf5f17c1e5ea537a683574619fabf14
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2023-03-23 11:06:30 -0300

    style: altera estilização da página principal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,10 @@
-import BackgroundHeader from '../../assets/fundo-bonito.jpg';
 import styles from './Header.module.css';
 
 function Header() {
   return (
     <header>
-      <div className={ styles.intro }>
-        <img
-          src={ BackgroundHeader }
-          className={ styles.imgHeader }
-          alt="imagem header"
-        />
-        <div className={ styles.title }>
-          <h1>Blogspot</h1>
-        </div>
+      <div className={ styles.title }>
+        <h1>Blogspot</h1>
       </div>
     </header>
   );
